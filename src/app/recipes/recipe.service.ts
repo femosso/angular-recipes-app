aa5547202afa7e92c0811d5d32aeb44f262a93f5
Felipe Mosso Ferfoglia <felipemosso@gmail.com>
2020-09-21 19:45:55 -0300

    Improving code with Routes and Observables

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
@@ -6,7 +6,7 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe(
             'A Test Recipe',
@@ -32,6 +32,10 @@ export class RecipeService {
 
     getRecipes() {
         return this.recipes.slice(); // return a new copy of this array
+    }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
     }
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
